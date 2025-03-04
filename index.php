c7c0c70bbb5c1d4c33851df2fbfba5204379ef3e
Sunil Kumar Sahoo <sibusonalilapi@gmail.com>
2025-03-05 00:16:56 +0530

    Modify The index page CDN Links To assets links

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>360 Feedback</title>
-  <!-- ----------------- Style CDN Links -----------------  -->
-  <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+  <!-- ----------------- Style Links -----------------  -->
+  <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="./assets/css/style.css">
 </head>
@@ -349,11 +349,9 @@
 
 
 
+  <!-- ------------------ Script Links --------------------  -->
 
-
-  <!-- ------------------ Script CDN Links --------------------  -->
-
-  <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="./assets/js/bootstrap.bundle.js"></script>
   <script src="/assets/js/main.js"></script>
 </body>
 
